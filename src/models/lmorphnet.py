de0c0186101f97539b773738d1046ad24d04c4fe
Romain Hermary <rhermary@lrde.epita.fr>
2022-11-04 23:49:58 +0100

    add double pconv lmoprh

--- src/models/lmorphnet.py
+++ src/models/lmorphnet.py
@@ -39,3 +39,39 @@ class LMorphNet(BaseNetwork):
         batch = self.sb1(batch)
 
         return batch
+
+class LMorphNetDouble(BaseNetwork):
+    """Network with one LMorph layer."""
+
+    def __init__(
+        self,
+        filter_size: Union[int, Tuple[int, int]],
+        loss_function: Callable,
+        **kwargs: Any,
+    ):
+        super().__init__(loss_function=loss_function)
+        self._set_hparams(
+            {
+                "filter_size": filter_size,
+                "loss_function": loss_function,
+                **kwargs,
+            }
+        )
+
+        self.lm1 = LMorph(
+            in_channels=1, out_channels=1, filter_size=filter_size, **kwargs
+        )
+        self.lm2 = LMorph(
+            in_channels=1, out_channels=1, filter_size=filter_size, **kwargs
+        )
+        self.sb1 = ScaleBias(1, **kwargs)
+
+    def forward(
+        self, batch: torch.Tensor, *args: Any, **kwargs: Any
+    ) -> torch.Tensor:
+        # pylint: disable=arguments-differ
+        batch = self.lm1(batch)
+        batch = self.lm2(batch)
+        batch = self.sb1(batch)
+
+        return batch
--- src/models/pconvnet.py
+++ src/models/pconvnet.py
@@ -39,3 +39,39 @@ class PConvNet(BaseNetwork):
         batch = self.sb1(batch)
 
         return batch
+
+class PConvNetDouble(BaseNetwork):
+    """Network with one PConv layer."""
+
+    def __init__(
+        self,
+        filter_size: int,
+        loss_function: Callable,
+        **kwargs: Any,
+    ):
+        super().__init__(loss_function=loss_function)
+        self._set_hparams(
+            {
+                "filter_size": filter_size,
+                "loss_function": loss_function,
+                **kwargs,
+            }
+        )
+
+        self.pconv1 = PConv(
+            in_channels=1, out_channels=1, filter_size=filter_size, **kwargs
+        )
+        self.pconv2 = PConv(
+            in_channels=1, out_channels=1, filter_size=filter_size, **kwargs
+        )
+        self.sb1 = ScaleBias(num_features=1, **kwargs)
+
+    def forward(
+        self, batch: torch.Tensor, *args: Any, **kwargs: Any
+    ) -> torch.Tensor:
+        # pylint: disable=arguments-differ
+        batch = self.pconv1(batch)
+        batch = self.pconv2(batch)
+        batch = self.sb1(batch)
+
+        return batch
